452b1c0cf575dd7ea03cfbb1864c6bbe9e02a39e
macbook <arvinmojaverian@hotmail.com>
2020-10-19 15:09:59 +0330

    Make Some Changes ... !

---
+++ My_Eshop/Migrations/20200825063257_AddMostTabeles.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace My_Eshop.Migrations
+{
+    public partial class AddMostTabeles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<decimal>(nullable: false),
+                    QuantityInStock = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    ItemId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "categoryToProducts",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoryToProducts", x => new { x.ProductId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_categoryToProducts_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_categoryToProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "ابزار کاربردی", "ابزار" },
+                    { 2, "نمامی برند لباس ها ", "لباس" },
+                    { 3, "تمامی برند های ساعت", "ساعت" },
+                    { 4, "لوازم", "لواز کاربردی" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categoryToProducts_CategoryId",
+                table: "categoryToProducts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ItemId",
+                table: "Products",
+                column: "ItemId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categoryToProducts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ My_Eshop/Migrations/20200825073020_AddDataBase.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace My_Eshop.Migrations
+{
+    public partial class AddDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Items",
+                type: "Money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "Price", "QuantityInStock" },
+                values: new object[,]
+                {
+                    { 1, 854.0m, 5 },
+                    { 2, 144.8m, 8 },
+                    { 3, 442.0m, 4 },
+                    { 4, 2500m, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ItemId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Asp.net آموزش", 1, "Asp.net Core 3" },
+                    { 2, "Blazor آموزش", 2, "Blazor‌" },
+                    { 3, "PWA آموزش", 3, "PWA" },
+                    { 4, "Entity آموزش", 4, "Entity" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "categoryToProducts",
+                columns: new[] { "ProductId", "CategoryId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 1, 4 },
+                    { 2, 1 },
+                    { 2, 2 },
+                    { 2, 3 },
+                    { 2, 4 },
+                    { 3, 1 },
+                    { 3, 2 },
+                    { 3, 3 },
+                    { 3, 4 },
+                    { 4, 1 },
+                    { 4, 2 },
+                    { 4, 3 },
+                    { 4, 4 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 2, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 3, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 4, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "categoryToProducts",
+                keyColumns: new[] { "ProductId", "CategoryId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Items",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "Money");
+        }
+    }
+}
